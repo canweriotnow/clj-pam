3a11c6255ec85bb567a9b260ec87903f9c5546b2
Jason Lewis <jason@decomplecting.org>
2013-03-08 19:03:28 -0500

    Ignore Vim swap files, update native deps

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [net.sf.jpam/jpam "1.1"]
-                 [org.clojars.canweriotnow/jpam-native-deps "1.1"]])
+                 [org.clojars.canweriotnow/jpam-native-deps "1.1.1"]])
